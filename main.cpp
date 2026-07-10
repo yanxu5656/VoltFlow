ed0db6466ff9dfc24c4c881c7800b21e7cff7aa2
Aleksib <lovecraft907@gmail.com>
2026-07-10 12:41:16 +0800

    VoltFlow

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QIcon>
+#include <QFile>
 #include "TaskManager.h"
 
 int main(int argc, char *argv[])
@@ -10,6 +12,9 @@ int main(int argc, char *argv[])
     TaskManager taskManager;
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("taskManager", &taskManager);
+    app.setWindowIcon(QIcon(":/qt/qml/VoltFlow/image/logo.ico"));
+    QIcon icon(":/qt/qml/VoltFlow/image/logo.png");
+    //qDebug() << "图标是否加载成功：" << !icon.isNull();
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
